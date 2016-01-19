7263d7a89a09612a940f76e437c3aaf586c814b0
jeffblack360 <jeffblack360@gmail.com>
2016-01-19 14:25:21 -0600

    Tweak add(), login(), and logout() methods; Reorder methods alphabetically.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -12,6 +12,29 @@ use Cake\Event\Event;
 class UsersController extends AppController
 {
     /**
+     * Add method
+     *
+     * @return void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $user = $this->Users->newEntity();
+        
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->data);
+            
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(['controller' => 'Users', 'action' => 'login']);
+            } else {
+                $this->Flash->error_1(__('The user could not be saved. Please, try again.'));
+            }
+        }
+        $this->set(compact('user'));
+        $this->set('_serialize', ['user']);
+    }
+    
+    /**
     * beforeFilter method
     *
     * @return void
@@ -21,81 +44,6 @@ class UsersController extends AppController
         parent::beforeFilter($event);
         $this->Auth->allow(['add','logout']);
         $this->set('options', ['admin' => 'Admin', 'author' => 'Author']);
-    }
-
-    /**
-    * Login method
-    *
-    * @return void
-    */
-    public function login() 
-    {
-        if ($this->request->is('post')) {
-            $user = $this->Auth->identify();
-            if ($user) {
-                $this->Auth->setUser($user);
-                return $this->redirect($this->Auth->redirectUrl());
-            }
-            $this->Flash->error(__('Invalid username or password, try again'));
-        }
-    }
-
-    /**
-    * Logout method
-    *
-    * @return void
-    */
-    public function logout() 
-    {
-        return $this->redirect($this->Auth->logout());
-    }
-    
-    /**
-     * Index method
-     *
-     * @return void
-     */
-    public function index()
-    {
-        $this->set('users', $this->paginate($this->Users));
-        $this->set('_serialize', ['users']);
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id User id.
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-        $this->set('user', $user);
-        $this->set('_serialize', ['user']);
-    }
-
-    /**
-     * Add method
-     *
-     * @return void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $user = $this->Users->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->data);
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The user could not be saved. Please, try again.'));
-            }
-        }
-        $this->set(compact('user'));
-        $this->set('_serialize', ['user']);
     }
 
     /**
@@ -140,5 +88,62 @@ class UsersController extends AppController
             $this->Flash->error(__('The user could not be deleted. Please, try again.'));
         }
         return $this->redirect(['action' => 'index']);
+    }
+
+    /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $this->set('users', $this->paginate($this->Users));
+        $this->set('_serialize', ['users']);
+    }
+
+    /**
+    * Login method
+    *
+    * @return void
+    */
+    public function login() 
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                $this->Flash->success(__('Login successful'));
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->set(__('Invalid username or password, try again'),[
+                'element' => 'error_1'
+            ]);
+        }
+    }
+
+    /**
+    * Logout method
+    *
+    * @return void
+    */
+    public function logout() 
+    {
+        return $this->redirect($this->Auth->logout());
+    }
+    
+    /**
+     * View method
+     *
+     * @param string|null $id User id.
+     * @return void
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => []
+        ]);
+        $this->set('user', $user);
+        $this->set('_serialize', ['user']);
     }
 }
