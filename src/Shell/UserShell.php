c1d92b150832c5a70a22e79bec76cc33a3554ed0
jeffblack360 <jeffblack360@gmail.com>
2016-02-11 09:07:31 -0600

    Testing UserShell setup for cron and model integration

--- src/Shell/UserShell.php
+++ src/Shell/UserShell.php
@@ -27,10 +27,30 @@ class UserShell extends Shell
     public function show()
     {
         if (empty($this->args[0])) {
-            return $this->error('Please enter a username.');
+//            return $this->error('Please enter a username.');
+            return $this->_showAll();
         }
         $user = $this->Users->findByUsername($this->args[0])->first();
         $this->out(print_r($user, true));
         
+    }
+    
+    /**
+    * _showAll() method
+    *
+    * Process all users
+    *
+    * @return void
+    */
+    protected function _showAll()
+    {
+        $users = $this->Users->find();
+        foreach ($users as $user) {
+            $this->log(
+                'username: ' . $user->username . ' ' .
+                'email: ' . $user->email, 'info'
+            );
+        }
+        return;
     }
 }
