78a67bc037919e410d279c65546a61934e698c60
jeffblack360 <jeffblack360@gmail.com>
2016-02-14 20:19:11 -0600

    Attach a UsersListener

--- config/events.php
+++ config/events.php
@@ -4,6 +4,7 @@ use Cake\Event\Event;
 use Cake\Event\EventManager;
 use Cake\Log\Log;
 use App\Model\Entity\User;
+use App\Event\UsersListener;
 
 /**
  * Attach custom events
@@ -13,3 +14,7 @@ use App\Model\Entity\User;
 EventManager::instance()->attach(function (Event $event, User $entity, ArrayObject $options) {
     Log::write('info', 'UsersTable.afterSave fired! '. $entity->username);
 }, 'UsersTable.afterSave');
+
+$UsersListener = new UsersListener();
+
+EventManager::instance()->attach($UsersListener);
