bdd4683458f6ba51f882491a5d7aad841ab7c234
jeffblack360 <jeffblack360@gmail.com>
2016-03-11 11:13:46 -0600

    Expand fixture

--- tests/Fixture/ArticlesFixture.php
+++ tests/Fixture/ArticlesFixture.php
@@ -40,10 +40,24 @@ class ArticlesFixture extends TestFixture
     public $records = [
         [
             'id' => 1,
-            'title' => 'Lorem ipsum dolor sit amet',
-            'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'title' => 'Article A',
+            'body' => 'Article A Body',
             'created' => '2015-08-13 22:27:17',
             'modified' => '2015-08-13 22:27:17'
+        ],
+                [
+            'id' => 2,
+            'title' => 'Article B',
+            'body' => 'Article B Body',
+            'created' => '2015-09-13 22:27:17',
+            'modified' => '2015-09-13 22:27:17'
+        ],
+                [
+            'id' => 3,
+            'title' => 'Article C',
+            'body' => 'Article C Body',
+            'created' => '2015-10-13 22:27:17',
+            'modified' => '2015-10-13 22:27:17'
         ],
     ];
 }
